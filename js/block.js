fd5cd56d580c5e51945ad667c3dad0b244e4df0b
Leandro Arellano <gingerbread2318@gmail.com>
2018-08-11 06:12:11 +0800

    Version 1.0.7

--- js/block.js
+++ js/block.js
@@ -1,6 +1,10 @@
 var Aboutcounter = 0;
 var paragraph1Counter = 0;
 var paragraph2Counter = 0;
+var expTitleCounter = 0;
+var expTitleDesc1Counter = 0;
+var expTitleDesc2Counter = 0;
+var expCompany1Counter = 0;
 $(window).scroll(function() {
     if (checkVisible($('#aboutTitle')) && Aboutcounter == 0) {
         showAbout();
@@ -9,8 +13,20 @@ $(window).scroll(function() {
         showParags();
         paragraph1Counter = 1;
     } else if (checkVisible($('#paragraphs2')) && paragraph2Counter == 0) {
-        showParags2()
+        showParags2();
         paragraph2Counter = 1;
+    } else if (checkVisible($('#appear')) && expTitleCounter == 0) {
+        showExpTitle();
+        expTitleCounter = 1;
+    } else if (checkVisible($('#latestExpDesc')) && expTitleDesc1Counter == 0) {
+        showExpDesc1();
+        expTitleDesc1Counter = 1;
+    } else if (checkVisible($('#latestExpDesc2')) && expTitleDesc2Counter == 0) {
+        showExpDesc2();
+        expTitleDesc2Counter = 1;
+    } else if (checkVisible($('#company1')) && expCompany1Counter == 0) {
+        showCompany1();
+        expCompany1Counter = 1;
     }
 });
 
@@ -35,4 +51,20 @@ function showParags() {
 
 function showParags2() {
     document.getElementById("paragraphs2").style.display = "block";
+}
+
+function showExpTitle() {
+    document.getElementById("expTitle").style.display = "block";
+}
+
+function showExpDesc1() {
+    document.getElementById("latestExpDesc").classList.add("pop");
+}
+
+function showExpDesc2() {
+    document.getElementById("latestExpDesc2").classList.add("pop");
+}
+
+function showCompany1() {
+    document.getElementById("company1").style.display = "block";
 }
